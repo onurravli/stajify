1e6800d9a7149a09fbbdfc560e703b95a21d6fec
onurravli <onurravli@hotmail.com>
2023-12-16 01:05:51 +0300

    feat(frontend): create right menu

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -2,8 +2,8 @@ import Hero from "@/components/hero";
 
 export default function Home() {
   return (
-    <main className="flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center p-4 mt-16">
-      <div className="flex align-middle justify-center items-center flex-col-reverse md:flex-row">
+    <main className="flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center px-8 py-16 mt-8">
+      <div className="flex align-middle justify-center items-center flex-col-reverse md:flex-row-reverse md:gap-24">
         <div className="max-w-4xl flex flex-col items-start gap-2 md:gap-4">
           <span className="text-2xl md:text-6xl font-semibold text-gray-800">
             staj bulmak hiç bu kadar kolay olmamıştı!
@@ -12,10 +12,10 @@ export default function Home() {
             stajify ile aradığın staja ulaşabilir, kariyerine yön verebilirsin.
           </span>
           <div className="w-full md:w-auto flex flex-col md:flex-row items-center align-middle justify-center gap-2 md:gap-4 mb-4">
-            <button className="w-full md:w-auto bg-blue-600 text-white md:py-4 py-2 md:px-12 px-6 text-xl border border-transparent transition-all duration-200 hover:bg-blue-800">
+            <button className="w-full md:w-auto bg-blue-600 text-white rounded-md py-2 md:px-12 px-6 text-xl border border-transparent transition-all duration-200 hover:bg-blue-800">
               keşfet
             </button>
-            <button className="w-full md:w-auto bg-transparent text-blue-600 md:py-4 py-2 md:px-12 px-6 text-xl border border-blue-600 transition-all duration-200 hover:bg-blue-600 hover:text-white">
+            <button className="w-full md:w-auto bg-transparent text-blue-600 rounded-md py-2 md:px-12 px-6 text-xl border border-blue-600 transition-all duration-200 hover:bg-blue-600 hover:text-white">
               daha fazlasını öğren
             </button>
           </div>
@@ -26,7 +26,7 @@ export default function Home() {
             </a>
           </span>
         </div>
-        <Hero className="w-4/5 md:w-3/5 md:min-w-[400px] h-auto md:hidden" />
+        <Hero className="w-4/5 md:w-3/5 md:min-w-[400px] h-auto" />
       </div>
     </main>
   );
--- packages/frontend/src/components/header.tsx
+++ packages/frontend/src/components/header.tsx
@@ -1,10 +1,36 @@
+"use client";
+
+import { useState } from "react";
 import Logo from "./logo";
+import { MenuIcon } from "lucide-react";
 
 export default function Header() {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <header className="w-full h-16 fixed top-0 left-0 bg-white text-black border-b">
-      <div className="w-full md:max-w-6xl h-16 flex flex-row items-center align-middle justify-center mx-auto px-4">
+      <div className="w-full h-16 flex flex-row items-center align-middle justify-center mx-auto px-4">
         <Logo className="h-8 w-auto" />
+        <button
+          className="h-16 w-16 fixed right-0 flex flex-row items-center align-middle justify-center"
+          onClick={() => {
+            setShowMenu(!showMenu);
+          }}
+        >
+          <MenuIcon className="h-8 w-auto text-blue-600" />
+        </button>
+        <span
+          onClick={() => {
+            setShowMenu(false);
+          }}
+          className={`w-full h-full fixed top-0 left-0 bg-black transition-all duration-200 ${
+            showMenu ? "opacity-40" : "opacity-0 pointer-events-none"
+          }`}
+        />
+        <nav
+          className={`w-full max-w-xs h-full bg-white fixed top-0 transition-all duration-200 ${
+            showMenu ? "opacity-100 right-0" : "opacity-0 pointer-events-none -right-full transition-all duration-200"
+          }`}
+        ></nav>
       </div>
     </header>
   );
