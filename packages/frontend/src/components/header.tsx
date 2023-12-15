4478e76761ef050b2162db6a033d9c267c355888
onurravli <onurravli@hotmail.com>
2023-12-16 01:26:44 +0300

    feat(frontend): update menu

--- packages/frontend/src/components/header.tsx
+++ packages/frontend/src/components/header.tsx
@@ -22,12 +22,12 @@ export default function Header() {
           onClick={() => {
             setShowMenu(false);
           }}
-          className={`w-full h-full fixed top-0 left-0 bg-black transition-all duration-200 ${
+          className={`w-full h-full fixed top-0 left-0 bg-black transition-all duration-700 ${
             showMenu ? "opacity-40" : "opacity-0 pointer-events-none"
           }`}
         />
         <nav
-          className={`w-full max-w-xs h-full bg-white fixed top-0 transition-all duration-200 ${
+          className={`w-full max-w-xs h-full bg-white fixed top-0 transition-all duration-300 ${
             showMenu ? "opacity-100 right-0" : "opacity-0 pointer-events-none -right-full transition-all duration-200"
           }`}
         ></nav>
