61d7ba067112850c9ac24bd3004aa7e5b12813f7
onurravli <onurravli@hotmail.com>
2023-12-17 00:12:20 +0300

    chore(frontend): update hero layout

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -7,14 +7,14 @@ export default function Home() {
     <main className="flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center px-8 py-24 mt-0">
       <div className="w-full md:max-w-7xl h-auto flex flex-col md:flex-row items-center align-middle justify-center md:gap-16">
         <Hero className="max-w-xs md:max-w-md w-full h-auto" />
-        <div className="flex flex-col items-stat justify-center gap-4">
-          <span className="text-4xl md:text-6xl font-semibold text-gray-800">
+        <div className="flex flex-col items-stat justify-center gap-2 md:gap-4">
+          <span className="text-2xl md:text-6xl font-semibold text-gray-800">
             staj bulmak hiç bu kadar kolay olmamıştı!
           </span>
-          <span className="text-2xl md:text-3xl font-normal text-gray-800">
+          <span className="text-1xl md:text-3xl font-normal text-gray-800">
             stajify ile aradığın staja ulaşabilir, kariyerine güzel bir başlangıç yapabilirsin.
           </span>
-          <div className="flex flex-col md:flex-row w-full items-center justify-start gap-4 mt-4">
+          <div className="flex flex-col md:flex-row w-full items-center justify-start gap-2 md:gap-4 mt-4">
             <Button size="lg" variant="primary">
               keşfet
             </Button>
