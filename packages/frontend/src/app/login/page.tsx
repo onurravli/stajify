61886561b7f26c8137ae1f471e4c81dc594351f4
onurravli <onurravli@hotmail.com>
2023-12-17 02:54:19 +0300

    chore(frontend): update login page

--- packages/frontend/src/app/login/page.tsx
+++ packages/frontend/src/app/login/page.tsx
@@ -1,3 +1,41 @@
+"use client";
+
+import Button from "@/components/button";
+import Input from "@/components/input";
+import { useState } from "react";
+
 export default function Login() {
-  return <main className="flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center px-8 py-24 mt-0"></main>;
+  const [email, setEmail] = useState("");
+  return (
+    <main className="flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center px-8 py-24 mt-0">
+      <form className="w-full md:max-w-md flex flex-col items-center align-middle justify-center gap-4">
+        <Input
+          placeholder="E-Mail Adresin"
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+          value={email}
+          autoComplete="email"
+        />
+        <Input
+          placeholder="Şifren"
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+          value={email}
+          type="password"
+          autoComplete="current-password"
+        />
+        <span className="w-full text-right">
+          <a href="#">Şifremi unuttum!</a>
+        </span>
+        <Button size="sm" variant="primary" className="w-full">
+          Giriş Yap
+        </Button>
+        <span>
+          Üye değil misin? <a href="/register">Kayıt ol!</a>
+        </span>
+      </form>
+    </main>
+  );
 }
