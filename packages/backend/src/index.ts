790a86439eb31211505871822c69a0d07837b120
onurravli <onurravli@hotmail.com>
2023-12-16 20:39:45 +0300

    chore(backend): update user table to add is_verified

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -2,11 +2,14 @@ import express from "express";
 import routes from "./routes";
 import cors from "cors";
 import { createCompaniesTable, createUsersTable } from "./services/postgres.service";
+import { config } from "dotenv";
 
 createCompaniesTable();
 createUsersTable();
 
 const app = express();
+const env = config();
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
@@ -16,4 +19,4 @@ app.use("/users", routes.usersRouter);
 app.use("/companies", routes.companiesRouter);
 app.use("*", routes.fallbackRouter);
 
-app.listen(3000);
+app.listen(port);
