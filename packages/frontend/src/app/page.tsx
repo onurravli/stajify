72e4973c53f63cc26000696322ca9e51392c08b5
onurravli <onurravli@hotmail.com>
2023-12-16 23:29:57 +0300

    chore(frontend): update hero

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -1,29 +1,30 @@
 import Button from "@/components/button";
 import Hero from "@/components/hero";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center px-8 py-16 mt-8">
-      <div className="flex align-middle justify-center items-center flex-col-reverse md:flex-row-reverse md:gap-24">
-        <div className="max-w-4xl flex flex-col items-start gap-2 md:gap-4">
-          <span className="text-2xl md:text-6xl font-semibold text-gray-800">
+    <main className="flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center px-8 py-24 mt-0">
+      <div className="w-full md:max-w-7xl h-auto flex flex-col md:flex-row items-center align-middle justify-center md:gap-16">
+        <Hero className="max-w-xs md:max-w-md w-full h-auto" />
+        <div className="flex flex-col items-stat justify-center gap-4">
+          <span className="text-4xl md:text-6xl font-semibold text-gray-800">
             staj bulmak hiç bu kadar kolay olmamıştı!
           </span>
-          <span className="text-xl md:text-3xl text-gray-800 mb-2">
-            stajify ile aradığın staja ulaşabilir, kariyerine yön verebilirsin.
+          <span className="text-2xl md:text-3xl font-normal text-gray-800">
+            stajify ile aradığın staja ulaşabilir, kariyerine güzel bir başlangıç yapabilirsin.
           </span>
-          <div className="w-full md:w-auto flex flex-col md:flex-row items-center align-middle justify-center gap-2 md:gap-4 mb-4">
+          <div className="flex flex-col md:flex-row w-full items-center justify-start gap-4 mt-4">
             <Button variant="primary">keşfet</Button>
-            <Button variant="secondary">daha fazlasını öğren</Button>
+            <Button variant="secondary">daha fazlası</Button>
           </div>
-          <span>
+          <span className="mt-4">
             işveren misiniz?{" "}
-            <a className="text-blue-600" href="#">
+            <Link className="text-blue-600" href="#">
               bizimle iletişime geçin.
-            </a>
+            </Link>
           </span>
         </div>
-        <Hero className="w-4/5 md:w-3/5 md:min-w-[400px] h-auto" />
       </div>
     </main>
   );
