c620550311835dc85b49c84e3c30688ad2e135ee
onurravli <onurravli@hotmail.com>
2023-12-16 22:30:30 +0300

    chore(frontend): update header style

--- packages/frontend/src/components/header.tsx
+++ packages/frontend/src/components/header.tsx
@@ -1,37 +1,61 @@
 "use client";
 
-import { useState } from "react";
-import Logo from "./logo";
 import { MenuIcon } from "lucide-react";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import Logo from "./logo";
+import Toggle from "./toggle";
+
+const links = [
+  {
+    href: "/login",
+    label: "giriş yap",
+  },
+  {
+    href: "/register",
+    label: "kayıt ol",
+  },
+];
 
 export default function Header() {
   const [showMenu, setShowMenu] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 20) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <header className="w-full h-16 fixed top-0 left-0 bg-white text-black border-b">
-      <div className="w-full h-16 flex flex-row items-center align-middle justify-center mx-auto px-4">
+    <header
+      className={`w-full fixed top-0 left-0 bg-white text-black transition-all duration-300 ${
+        scrolled ? "h-16 shadow-lg" : "h-20"
+      }`}
+    >
+      <div
+        className={`w-full md:max-w-7xl h-full flex flex-row items-center align-middle justify-between mx-auto  transition-all duration-200 ${
+          scrolled ? "px-4" : "px-6"
+        }`}
+      >
         <Logo className="h-8 w-auto" />
-        <button
-          aria-label="Menu"
-          className="h-16 w-16 fixed right-0 flex flex-row items-center align-middle justify-center"
-          onClick={() => {
-            setShowMenu(!showMenu);
-          }}
-        >
-          <MenuIcon className="h-8 w-auto text-blue-600" />
-        </button>
-        <span
-          onClick={() => {
-            setShowMenu(false);
-          }}
-          className={`w-full h-full fixed top-0 left-0 bg-black transition-all duration-700 ${
-            showMenu ? "opacity-40" : "opacity-0 pointer-events-none"
-          }`}
-        />
-        <nav
-          className={`w-full max-w-xs h-full bg-white fixed top-0 transition-all duration-300 ${
-            showMenu ? "opacity-100 right-0" : "opacity-0 pointer-events-none -right-full transition-all duration-200"
-          }`}
-        ></nav>
+        <nav>
+          <ul className="flex items-center align-middle justify-center gap-4">
+            {links.map((link) => {
+              return (
+                <li key={link.href}>
+                  <Link className="text-lg transition-all duration-200 hover:text-blue-600" href={link.href}>
+                    {link.label}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
       </div>
     </header>
   );
