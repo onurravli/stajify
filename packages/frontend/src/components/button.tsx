c9650d1068cff273368d41a2b87752ca0e3a0884
onurravli <onurravli@hotmail.com>
2023-12-17 01:50:19 +0300

    feat(frontend): update button style

--- packages/frontend/src/components/button.tsx
+++ packages/frontend/src/components/button.tsx
@@ -2,29 +2,44 @@ export default function Button({
   children,
   variant,
   size,
+  onClick,
+  className,
 }: {
   children?: React.ReactNode;
   variant?: "primary" | "secondary";
-  size?: "sm" | "md" | "lg";
+  size?: "sm" | "md" | "lg" | "icon";
+  onClick?: () => void;
+  className?: string;
 }) {
   return (
     <button
-      className={`w-full md:w-auto rounded-md border transition-all duration-200 ${
+      className={`w-full md:w-auto rounded-md border transition-all duration-200 text-sm 
+      ${
         variant == "primary"
           ? "border-transparent bg-blue-600 text-white hover:bg-blue-800"
           : variant == "secondary"
           ? "border-blue-600 bg-transparent text-blue-600 hover:bg-blue-50"
           : ""
       } ${
+        // size == "sm"
+        //   ? "px-4 py-2 text-sm"
+        //   : size == "md"
+        //   ? "px-6 py-3 text-lg"
+        //   : size == "lg"
+        //   ? "px-10 py-3 text-xl"
+        //   : ""
         size == "sm"
-          ? "px-4 py-2 text-sm"
+          ? "h-10 text-sm px-4 py-2"
           : size == "md"
-          ? "px-6 py-3 text-lg"
+          ? "h-12 text-lg px-6 py-3"
           : size == "lg"
-          ? "px-10 py-3 text-xl"
+          ? "h-14 text-xl px-10 py-3"
+          : size == "icon"
+          ? "h-10 w-10 flex flex-row items-center justify-center align-middle bg-blue-600 text-white border-blue-600"
           : ""
-      }
+      } ${className}
       `}
+      onClick={onClick}
     >
       {children}
     </button>
