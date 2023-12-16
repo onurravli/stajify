652720dbc77183ab59148b53e90f52bf52b9128e
onurravli <onurravli@hotmail.com>
2023-12-17 00:16:43 +0300

    chore(frontend): update hero layout

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center px-8 py-24 mt-0">
       <div className="w-full md:max-w-7xl h-auto flex flex-col md:flex-row items-center align-middle justify-center md:gap-16">
-        <Hero className="max-w-xs md:max-w-md w-full h-auto" />
+        <Hero className="max-w-[15rem] md:max-w-md w-full h-auto" />
         <div className="flex flex-col items-stat justify-center gap-2 md:gap-4">
           <span className="text-2xl md:text-6xl font-semibold text-gray-800">
             staj bulmak hiç bu kadar kolay olmamıştı!
