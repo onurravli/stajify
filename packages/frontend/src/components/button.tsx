e22a0c60761cab1820889f9c352eb8d032d15fa8
onurravli <onurravli@hotmail.com>
2023-12-16 23:40:49 +0300

    chore(frontend): update button style

--- packages/frontend/src/components/button.tsx
+++ packages/frontend/src/components/button.tsx
@@ -1,19 +1,30 @@
 export default function Button({
   children,
   variant,
+  size,
 }: {
   children?: React.ReactNode;
   variant?: "primary" | "secondary";
+  size?: "sm" | "md" | "lg";
 }) {
   return (
     <button
-      className={`w-full md:w-auto rounded-md py-2 md:px-12 px-6 text-xl border transition-all duration-200 ${
+      className={`w-full md:w-auto rounded-md border transition-all duration-200 ${
         variant == "primary"
           ? "border-transparent bg-blue-600 text-white hover:bg-blue-800"
           : variant == "secondary"
-          ? "border-blue-600 bg-transparent text-blue-600 hover:bg-blue-600 hover:text-white"
+          ? "border-blue-600 bg-transparent text-blue-600 hover:bg-blue-50"
           : ""
-      }`}
+      } ${
+        size == "sm"
+          ? "px-4 py-2 text-sm"
+          : size == "md"
+          ? "px-6 py-3 text-lg"
+          : size == "lg"
+          ? "px-10 py-3 text-xl"
+          : ""
+      }
+      `}
     >
       {children}
     </button>
