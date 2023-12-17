ba2dfec82f539d76a1d3186c4f5457e285e5223f
Onur Ravli <onur@onurravli.com>
2023-12-18 02:00:12 +0300

    chore(frontend): update layout

--- packages/frontend/src/app/register/page.tsx
+++ packages/frontend/src/app/register/page.tsx
@@ -38,7 +38,7 @@ export default function Register() {
     }
   };
   return (
-    <main className="flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center px-8 py-24 mt-16">
+    <main className="flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center px-8 py-24">
       <motion.form
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
