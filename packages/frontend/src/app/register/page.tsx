2a4e86d2136f5a19b1d2710e4e534cb00ee743e9
onurravli <onurravli@hotmail.com>
2023-12-17 22:38:56 +0300

    feat(frontend): update register page

--- packages/frontend/src/app/register/page.tsx
+++ packages/frontend/src/app/register/page.tsx
@@ -1,3 +1,99 @@
+"use client";
+
+import React, { useState } from "react";
+import axios, { Axios, AxiosError } from "axios";
+import Input from "@/components/input";
+import Button from "@/components/button";
+import { useRouter } from "next/navigation";
+
 export default function Register() {
-  return <main className="flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center px-8 py-24 mt-0"></main>;
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+  const handleRegister = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/users/`, {
+        name,
+        surname,
+        phone,
+        email,
+        password,
+      });
+      alert(res.data["messagePrintable"]);
+      router.push("/login");
+    } catch (err: unknown) {
+      if (err instanceof AxiosError) {
+        alert(err.response?.data["errorPrintable"]);
+      }
+    }
+  };
+  return (
+    <main className="flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center px-8 py-24 mt-10">
+      <form
+        className="w-full md:max-w-md flex flex-col items-center align-middle justify-center gap-4"
+        onSubmit={handleRegister}
+      >
+        <div className="w-full flex flex-row gap-4">
+          <Input
+            placeholder="Adın"
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+            value={name}
+            type="name"
+            required
+          />
+          <Input
+            placeholder="Soyadın"
+            onChange={(event) => {
+              setSurname(event.target.value);
+            }}
+            value={surname}
+            type="surname"
+            required
+          />
+        </div>
+        <Input
+          placeholder="E-Mail Adresin"
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+          value={email}
+          autoComplete="email"
+          type="email"
+          required
+        />
+        <Input
+          placeholder="Telefon Numaran"
+          onChange={(event) => {
+            setPhone(event.target.value);
+          }}
+          value={phone}
+          type="tel"
+          required
+          autoComplete="off"
+        />
+        <Input
+          placeholder="Şifren"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+          value={password}
+          autoComplete="current-password"
+          type="password"
+          required
+        />
+        <Button size="sm" variant="primary" className="w-full" type="submit">
+          <span className="text-base">Kayıt Ol</span>
+        </Button>
+        <span>
+          Zaten üye misin? <a href="/login">Giriş Yap</a>
+        </span>
+      </form>
+    </main>
+  );
 }
