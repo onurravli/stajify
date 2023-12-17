bdc7d3a88a2c3a27e6c26d3e6d6543ba33900b95
onurravli <onurravli@hotmail.com>
2023-12-17 22:41:11 +0300

    chore(backend): middleware

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -2,7 +2,8 @@ import express from "express";
 import routes from "./routes";
 import cors from "cors";
 import { createCompaniesTable, createUsersTable, tryToConnect } from "./services/postgres.service";
-import { config } from "dotenv";
+import * as dotenv from "dotenv";
+import bodyParser from "body-parser";
 
 tryToConnect().catch((err) => {
   return "Couldn't connect to DB.";
@@ -12,11 +13,13 @@ createCompaniesTable();
 createUsersTable();
 
 const app = express();
-const env = config();
+const env = dotenv.config();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", routes.rootRouter);
 app.use("/users", routes.usersRouter);
