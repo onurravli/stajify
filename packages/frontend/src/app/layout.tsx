aff69f28eafb2f2af17c704b9a0fdf5c829ae78c
onurravli <onurravli@hotmail.com>
2023-12-17 17:45:31 +0300

    chore(frontend): og meta tags

--- packages/frontend/src/app/layout.tsx
+++ packages/frontend/src/app/layout.tsx
@@ -7,9 +7,62 @@ const montserrat = Montserrat({
   subsets: ["latin-ext"],
   weight: ["300", "400", "500", "600", "700", "800", "900"],
 });
+
 export const metadata: Metadata = {
   title: "Stajify | Anasayfa",
-  description: "Staj bulmak hiç bu kadar kolay olmamıştı.",
+  description: "Staj bulmak hiç bu kadar kolay olmamıştı!",
+  metadataBase: new URL("https://stajify.com"),
+  keywords: [
+    "Stajify",
+    "staj",
+    "bulma",
+    "uygulama",
+    "stajyer",
+    "iş",
+    "öğrenci",
+    "kariyer",
+    "gelişim",
+    "iş deneyimi",
+    "öğrenci stajları",
+    "stajyerlik programı",
+    "iş fırsatları",
+    "meslek stajı",
+    "stajyer bulma",
+    "stajyerlik",
+    "stajyer arama",
+    "stajyer iş ilanları",
+  ],
+  openGraph: {
+    title: "Stajify",
+    description: "Staj bulmak hiç bu kadar kolay olmamıştı!",
+    url: "https://stajify.com",
+    siteName: "Stajify",
+    images: [
+      {
+        url: "https://stajify.com/og.png",
+      },
+    ],
+    locale: "tr_TR",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      "max-image-preview": "large",
+    },
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Stajify",
+    description: "Staj bulmak hiç bu kadar kolay olmamıştı!",
+    images: ["https://stajify.com/og.png"],
+  },
+  category: "technology",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
