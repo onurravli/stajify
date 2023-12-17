5fe7acb9418ab904fdc452798f653104455a6c09
onurravli <onurravli@hotmail.com>
2023-12-18 00:36:28 +0300

    chore(frontend): create about page

---
+++ packages/frontend/src/app/about/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import React, { useState } from "react";
+
+export default function Contact() {
+  return <main className="flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center px-8 py-24 mt-16"></main>;
+}
