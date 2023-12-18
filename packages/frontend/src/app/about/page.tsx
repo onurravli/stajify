a1f893a5bc766880ef4078f3ab9fe98f4991fa36
onurravli <onurravli@hotmail.com>
2023-12-18 12:52:18 +0300

    chore(frontend): update layout

--- packages/frontend/src/app/about/page.tsx
+++ packages/frontend/src/app/about/page.tsx
@@ -3,5 +3,5 @@
 import React, { useState } from "react";
 
 export default function Contact() {
-  return <main className="flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center px-8 py-24 mt-16"></main>;
+  return <main className="flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center px-8 py-24"></main>;
 }
